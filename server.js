fdcf67ad2a2f4154c059d0ef81e0cac951046f91
Abhilash Bhardwaj <abhilashnco@gmail.com>
2016-12-24 00:14:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(morgan('short'));
 app.get('/', function(req, res) {
-    res.sendFile(__dirname, +'/index.html');
+    res.sendFile(__dirname +'/index.html');
 });
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
