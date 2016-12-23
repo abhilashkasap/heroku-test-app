5f71f12ffde0df0218de170039144d5ac849185c
VIKSHIPT <VIKSHIPT>
2016-12-23 21:44:52 +0530

    made changes

---
+++ server.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var morgan = require('morgan');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(morgan('short'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname, +'/index.html');
+});
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
